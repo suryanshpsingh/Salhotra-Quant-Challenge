7223707701d69e30dad4c2b54386cb9d56286ba2
Dpmon1 <shreyjoshi3103@gmail.com>
2020-11-19 15:53:05 -0500

    Code to test

--- main.py
+++ main.py
@@ -23,12 +23,13 @@ class SGSTeamAlgorithm(QCAlgorithm):
         self.stocksBudget = 0.2
         
         # Equities:
-        self.initialStocks = ["TSLA", "ABT", "ABBV", "ABMD", "ACN", "ATVI", "ADBE", "AMD", "AAP", "AES", "AFL", "A", "APD", "AKAM", "ALK", "ALB", "ARE", "ALXN", "ALGN", "ALLE", "ALL", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME", "AMGN", "APH", "ADI", "ANSS", "ANTM", "AON", "AOS", "APA", "AIV", "AAPL", "AMAT", "APTV", "ADM", "ANET", "AJG", "AIZ", "ATO", "ADSK", "ADP", "AZO", "AVB", "AVY", "AVGO", "BKR", "BLL", "BAC", "BK", "BAX", "BDX", "BRK.B", "BBY", "BIO", "BIIB", "BLK", "BA", "BKNG", "BWA", "BXP", "BSX", "BMY", "BR", "BF.B", "BEN", "CHRW", "COG", "CDNS", "CPB", "COF", "CAH", "CCL", "CAT", "CBOE", "CDW", "CE", "CNC", "CNP", "CTL", "CERN", "CF", "CHTR", "CVX", "CMG", "CB", "CHD", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CTXS", "CLX", "CME", "CMS", "CTSH", "CL", "CMCSA", "CMA", "CAG", "CXO", "COP", "COO", "CPRT", "COST", "COTY", "CCI", "CSX", "CMI", "CVS", "CRM", "DHI", "DHR", "DRI", "DVA", "DE", "DAL", "DVN", "DXCM", "DLR", "DFS", "DISCA", "DISCK", "DISH", "DG", "DLTR", "D", "DPZ", "DOV", "DTE", "DUK", "DRE", "DD", "DXC", "DGX", "DIS", "ED", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMR", "ETR", "EOG", "EFX", "EQIX", "EQR", "ESS", "EL", "EVRG", "ES", "EXC", "EXPE", "EXPD", "EXR", "FANG", "FFIV", "FB", "FAST", "FRT", "FDX", "FIS", "FITB", "FE", "FRC", "FISV", "FLT", "FLIR", "FLS", "FMC", "F", "FTNT", "FTV", "FBHS", "FCX", "FTI", "GOOGL", "GOOG", "GLW", "GPS", "GRMN", "GD", "GE", "GIS", "GM", "GPC", "GILD", "GL", "GPN", "GS", "GWW", "HRB", "HAL", "HBI", "HIG", "HAS", "HCA", "HSIC", "HSY", "HES", "HPE", "HLT", "HFC", "HOLX", "HD", "HON", "HRL", "HST", "HWM", "HPQ", "HUM", "HBAN", "HII", "IT", "IEX", "IDXX", "INFO", "ITW", "ILMN", "INCY", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IPGP", "IQV", "IRM", "JKHY", "JBHT", "JNJ", "JCI", "JPM", "JNPR", "KMX", "KO", "KSU", "K", "KEY", "KEYS", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LNT", "LB", "LHX", "LH", "LRCX", "LW", "LVS", "LEG", "LDOS", "LEN", "LLY", "LNC", "LIN", "LYV", "LKQ", "LMT", "L", "LOW", "LYB", "LUV", "MMM", "MO", "MTB", "MRO", "MPC", "MKTX", "MAR", "MMC", "MLM", "MAS", "MA", "MKC", "MXIM", "MCD", "MCK", "MDT", "MRK", "MET", "MTD", "MGM", "MCHP", "MU", "MSFT", "MAA", "MHK", "MDLZ", "MNST", "MCO", "MS", "MOS", "MSI", "MSCI", "MYL", "NDAQ", "NOV", "NTAP", "NFLX", "NWL", "NEM", "NWSA", "NWS", "NEE", "NLSN", "NKE", "NI", "NBL", "NSC", "NTRS", "NOC", "NLOK", "NCLH", "NRG", "NUE", "NVDA", "NVR", "NOW", "ORLY", "OXY", "ODFL", "OMC", "OKE", "ORCL", "O", "PCAR", "PKG", "PH", "PAYX", "PAYC", "PYPL", "PNR", "PBCT", "PEP", "PKI", "PRGO", "PFE", "PM", "PSX", "PNW", "PXD", "PNC", "PPG", "PPL", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "PWR", "QRVO", "QCOM", "RE", "RL", "RJF", "REG", "REGN", "RF", "RSG", "RMD", "RHI", "ROK", "ROL", "ROP", "ROST", "RCL", "SCHW", "STZ", "SJM", "SPGI", "SBAC", "SLB", "STX", "SEE", "SRE", "SHW", "SPG", "SWKS", "SLG", "SNA", "SO", "SWK", "SBUX", "STT", "STE", "SYK", "SIVB", "SYF", "SNPS", "SYY", "T", "TAP", "TMUS", "TROW", "TTWO", "TPR", "TGT", "TEL", "TDY", "TFX", "TXN", "TXT", "TMO", "TIF", "TJX", "TSCO", "TT", "TDG", "TRV", "TWTR", "TYL", "TSN", "UDR", "ULTA", "USB", "UAA", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UHS", "UNM", "VFC", "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "V", "VNO", "VMC", "WRB", "WAB", "WMT", "WBA", "WM", "WAT", "WEC", "WFC", "WELL", "WST", "WDC", "WU", "WRK", "WY", "WHR", "WMB", "WLTW", "WYNN", "XRAY", "XOM", "XEL", "XRX", "XLNX", "XYL", "YUM", "ZBRA", "ZBH", "ZION", "ZTS"]
+        self.initialStocks = ["TSLA", "AMZN", "AAPL", "GOOG", "BABA", "FB", "TWTR", "IBM", "ABT", "ABBV", "CVS", "GD", "GE", "GIS", "GM"]
         self.stocks = []
         self.sortedTenStocks = []
         # stocks[rank] = [[stock, option], isReady, [previous price, current price], stockReturn]
 
         # Setup Loops
+        x = 0
         for stock in self.initialStocks:
             self.stocks.append([
                 [
@@ -41,7 +42,7 @@ class SGSTeamAlgorithm(QCAlgorithm):
             ])
             self.stocks[x-1][0][0].SetDataNormalizationMode(DataNormalizationMode.Raw)
             self.stocks[x-1][0][1].SetFilter(lambda u: u.Expiration(1, 300))
-
+            x = x + 1
         # Gather prices: worst case takes 9 minutes (each, exclusive - if one is worst then other is not).
         # Previous Price 
         # at 9:30 am today
@@ -124,6 +125,18 @@ class SGSTeamAlgorithm(QCAlgorithm):
             stock[3] = (stock[2][1] - stock[2][0])/stock[2][0]
 
     # Trading helpers:
+    def isCall(u):
+        if u.Right == OptionRight.Call:
+            return True
+        else:
+            return False
+
+    def isPut(u):
+        if u.Right == OptionRight.Put:
+            return True
+        else:
+            return False
+
     def buyOptions(self, amount, stock, type):
         # type = "Call" OR "Put"
         # Get chain
@@ -133,23 +146,10 @@ class SGSTeamAlgorithm(QCAlgorithm):
         if len(contractChain) == 0: return
 
         if type == "Call":
-            contractChain = filter((
-                lambda u: (
-                    if u.Right == OptionRight.Call:
-                        return True
-                    else:
-                        return False
-                )
-            ), contractChain)
+            contractChain = filter(isCall, contractChain)
+
         elif type == "Put":
-            contractChain = filter((
-                lambda u: (
-                    if u.Right == OptionRight.Put:
-                        return True
-                    else:
-                        return False
-                )
-            ), contractChain)
+            contractChain = filter(isPut, contractChain)
 
         self.MarketOrder(contractChain[0].Symbol, amount/contractChain[0].Price) 
         
